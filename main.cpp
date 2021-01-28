8b07ce0fdece66cc657f0bc21473f4ca7017bc80
王莉莉 <lily@wanglilideMacBook.local>
2021-01-28 22:47:00 +0800

    error commit

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 
 int main()
 {
+<<<<<<< Updated upstream
     std::cout << "hi world!" << std::endl;
+=======
+    std::cout << "hello mac!" << std::endl;
+>>>>>>> Stashed changes
     return 0;
 }
